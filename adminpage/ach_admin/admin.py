331cd1e85dfcfd99829bbe93f4e9dec88c907c0a
Alexandra Vabnits <a.vabnits@innopolis.university>
2023-06-29 01:40:46 +0300

    rollback admin feature

--- adminpage/ach_admin/admin.py
+++ adminpage/ach_admin/admin.py
@@ -10,23 +10,23 @@ from .models import Question
 from .models import Achievement, AchTeacher, AchStudent
 
 
-#class achievementPage(admin.ModelAdmin):
-#    change_list_template = 'ach_admin/admin.html'
-#    filter_horizontal = ('subscribed_students', 'assigned_coaches', 'finished_students',)
-#
-#
-#class teacherPage(admin.ModelAdmin):
-#    filter_horizontal = ('assigned_achievements',)
-#
-#
-#class studentPage(admin.ModelAdmin):
-#    filter_horizontal = ('finished_achievements', 'current_achievements',)
-#
+class achievementPage(admin.ModelAdmin):
+    change_list_template = 'ach_admin/admin.html'
+    filter_horizontal = ('subscribed_students', 'assigned_coaches', 'finished_students',)
+
+
+class teacherPage(admin.ModelAdmin):
+    filter_horizontal = ('assigned_achievements',)
+
+
+class studentPage(admin.ModelAdmin):
+    filter_horizontal = ('finished_achievements', 'current_achievements',)
+
 
 # admin.site.register(Question)
-#admin.site.register(Achievement, achievementPage)
-#admin.site.register(AchTeacher, teacherPage)
-#admin.site.register(AchStudent, studentPage)
-admin.site.register(Achievement)
-admin.site.register(AchTeacher)
-admin.site.register(AchStudent)
+admin.site.register(Achievement, achievementPage)
+admin.site.register(AchTeacher, teacherPage)
+admin.site.register(AchStudent, studentPage)
+#admin.site.register(Achievement)
+#admin.site.register(AchTeacher)
+#admin.site.register(AchStudent)
