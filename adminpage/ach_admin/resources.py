bf53b289446330f96923041186f776da16139ef1
Alexandra Vabnits <a.vabnits@innopolis.university>
2023-07-16 21:52:10 +0300

    fix import function

--- adminpage/ach_admin/resources.py
+++ adminpage/ach_admin/resources.py
@@ -29,36 +29,86 @@ class AchievementAchStudentResource(resources.ModelResource):
     )
     date_achieved = fields.Field(
         column_name='Date Achieved',
-        attribute='date_achieved'
+        attribute='date_achieved',
     )
     status = fields.Field(
         column_name='Status',
         attribute='status'
     )
 
+    # def before_import_row(self, row, **kwargs):
+    #     # Convert achievement title to achievement object
+    #     achievement_title = row.get('Achievement Title')
+    #     if achievement_title:
+    #         achievement = Achievement.objects.filter(title=achievement_title).first()
+    #         row['Achievement Title'] = achievement
+    #
+    #     # Convert ach_student email to ach_student object
+    #     ach_student_email = row.get('Student Email')
+    #     if ach_student_email:
+    #         ach_student = AchStudent.objects.filter(user__email=ach_student_email).first()
+    #         row['Student Email'] = ach_student
+    #
+    #     # Update the 'status' attribute with the value from the 'Status' column
+    #     status = row.get('Status')
+    #     if status:
+    #         row['Status'] = status
+    #
+    #     # Update the 'date_achieved' attribute with the value from the 'Date Achieved' column
+    #     date_achieved = row.get('Date Achieved')
+    #     if date_achieved:
+    #         row['Date Achieved'] = date_achieved
+    #
+    #     # Check if the relation already exists in the database
+    #     achievement = row.get('Achievement Title')
+    #     ach_student = row.get('Student Email')
+    #     if achievement and ach_student:
+    #         existing_instance = AchievementAchStudent.objects.filter(
+    #             achievement=achievement,
+    #             ach_student=ach_student
+    #         ).first()
+    #         if existing_instance:
+    #             # Update the existing instance with the new values
+    #             for key, value in row.items():
+    #                 setattr(existing_instance, key, value)
+    #             # Save the updated instance
+    #             existing_instance.save()
+    #             # Skip importing the row by returning None
+    #             return None
+
+
     def before_import_row(self, row, **kwargs):
-        # Convert achievement title to achievement object
         achievement_title = row.get('Achievement Title')
-        if achievement_title:
-            achievement = Achievement.objects.filter(title=achievement_title).first()
-            row['Achievement Title'] = achievement
-
-        # Convert ach_student email to ach_student object
         ach_student_email = row.get('Student Email')
-        if ach_student_email:
-            ach_student = AchStudent.objects.filter(user__email=ach_student_email).first()
-            row['Ach Student Email'] = ach_student
-
-        # Update the 'status' attribute with the value from the 'Status' column
         status = row.get('Status')
-        if status:
-            row['status'] = status
-
         date_achieved = row.get('Date Achieved')
-        if date_achieved:
-            row['date_achieved'] = date_achieved
-        else:
-            row['date_achieved'] = datetime.date.today()
+
+        # Check if the record already exists in the database
+        existing_instance = AchievementAchStudent.objects.filter(
+            achievement__title=achievement_title,
+            ach_student__user__email=ach_student_email
+        ).first()
+
+        if existing_instance:
+            # Delete the existing instance
+            existing_instance.delete()
+            # update tag
+
+        # Continue with importing the row
+        return row
+
+    def before_save_instance(self, instance, using_transactions, dry_run):
+        # Check if the record already exists in the database
+        existing_instance = AchievementAchStudent.objects.filter(
+            achievement=instance.achievement,
+            ach_student=instance.ach_student
+        ).first()
+
+        if existing_instance:
+            # Delete the existing instance
+            existing_instance.delete()
+
+        return instance
 
     class Meta:
         model = AchievementAchStudent
@@ -66,4 +116,4 @@ class AchievementAchStudentResource(resources.ModelResource):
         import_id_fields = ('achievement_title', 'ach_student_email', 'date_achieved')
         export_order = ('achievement_title', 'ach_student_email', 'date_achieved', 'status')
         skip_unchanged = True
-        report_skipped = False
+        report_skipped = True
